33ae10360b4eee784796bdf9f69a35d022dadf03
Bartosz Gałek <bartosz@galek.com.pl>
2022-08-30 01:20:23 +0200

    initial version

---
+++ build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+    jacoco
+    id("pl.allegro.tech.build.axion-release") version "1.14.0"
+    id("com.adarshr.test-logger") version "3.2.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:31.0.1-jre")
+}
+
+group = "com.github.bgalek.security.svg"
+version = scmVersion.version
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter("5.8.2")
+        }
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.6"
+    reportsDir = file("$buildDir/reports/jacoco")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        xml.destination = file("$buildDir/reports/jacoco/report.xml")
+        csv.isEnabled = false
+        html.isEnabled = false
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("sonatype") {
+            from(components["java"])
+            pom {
+                name.set("")
+                description.set("")
+                url.set("https://github.com/bgalek/")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("bgalek")
+                        name.set("Bartosz Gałek")
+                        email.set("bartosz@galek.com.pl")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/bgalek/.git")
+                    developerConnection.set("scm:git:ssh://github.com:bgalek/.git")
+                    url.set("https://github.com/bgalek/safe-svg/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
+
+System.getenv("GPG_KEY_ID")?.let {
+    signing {
+        useInMemoryPgpKeys(
+            System.getenv("GPG_KEY_ID"),
+            System.getenv("GPG_PRIVATE_KEY"),
+            System.getenv("GPG_PRIVATE_KEY_PASSWORD")
+        )
+        sign(publishing.publications)
+    }
+}
